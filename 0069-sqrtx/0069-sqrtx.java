7910e2dbf7b36266833cacce550ba1289e6258e2
JOY <141978977+joy226@users.noreply.github.com>
2024-09-13 01:04:15 +0530

    Time: 1 ms (85.84%), Space: 40.8 MB (41.37%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -3,10 +3,32 @@ class Solution {
         if (x == 0) {
             return 0;
         }
-        long result = 1;
-        while (result * result <= x) {
-            result++;
+
+        // linear search:
+        // long result = 1;
+        // while (result * result <= x) {
+        //     result++;
+        // }
+        // return (int) (result - 1);
+        // ################################
+
+
+        // Binary Search:
+        long start = 1;
+        long end = x;
+        while(start <= end){
+            long mid = start + (end - start) / 2;
+
+            if(mid * mid > x){
+                end = mid - 1;
+            }
+            else if(mid * mid < x){
+                start = mid + 1;
+            }
+            else{
+                return (int) mid;
+            }
         }
-        return (int) (result - 1);
+        return (int) (start - 1);
     }
 }
