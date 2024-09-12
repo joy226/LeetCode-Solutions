18f70bfb77a8b6625cb2045c483523fcc645bc39
JOY <141978977+joy226@users.noreply.github.com>
2024-09-12 11:22:38 +0530

    Time: 1 ms (98.57%), Space: 43.2 MB (20.02%) - LeetHub

--- 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.java
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.java
@@ -19,6 +19,13 @@ class Solution {
 
     public boolean even(int num){
         int count = 0;
+        if(num < 0){
+            num = num * (-1);
+        }
+        if(num == 0){
+            count = 1;
+        }
+        // count = (int)(Math.log10(num))+1;
         while(num>0){
             count++;
             num = num/10;
