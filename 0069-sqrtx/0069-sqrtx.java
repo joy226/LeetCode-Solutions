9fcde07d6dcd49fa85422247455d7e93712d6df4
JOY <141978977+joy226@users.noreply.github.com>
2024-09-12 23:43:40 +0530

    Time: 19 ms (10.16%), Space: 40.5 MB (94.73%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -3,25 +3,10 @@ class Solution {
         if (x == 0) {
             return 0;
         }
-        // Initialize left and right pointers
-        long left = 1;
-        long right = x;
-
-        // Perform binary search
-        while (left <= right) {
-            long mid = left + (right - left) / 2;
-            long square = mid * mid;
-
-            if (square == x) {
-                return (int) mid; // Exact square root found
-            } else if (square > x) {
-                right = mid - 1; // Square is too large
-            } else {
-                left = mid + 1; // Square is too small
-            }
+        long result = 1;
+        while (result * result <= x) {
+            result++;
         }
-
-        // If exact square root not found, return the largest integer whose square is less than or equal to x
-        return (int) (left - 1);
+        return (int) (result - 1);
     }
 }
