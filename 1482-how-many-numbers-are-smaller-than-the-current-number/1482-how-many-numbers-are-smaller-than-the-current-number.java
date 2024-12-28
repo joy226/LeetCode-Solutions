b0f90ba85612b98c69eb8d84692a93149a317956
JOY <141978977+joy226@users.noreply.github.com>
2024-12-28 16:24:54 +0530

    Time: 9 ms (32.31%), Space: 44.5 MB (58.86%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,21 +1,31 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
         int[] ans = new int[nums.length];
-        return helper(nums, 0, 0, 1, ans);
-    }
-
-    public int[] helper(int[] nums, int i, int count, int j, int[] ans) {
-        if(i == nums.length) {
-            return ans;
-        }
-        if(nums[i] > nums[j] && nums[i] != nums[j]) {
-            count++;
-        }
-        if(j == nums.length - 1) {
+        // return helper(nums, 0, 0, 1, ans);
+        for(int i = 0; i < nums.length; i++) {
+            int count = 0;
+            for(int j = 0; j < nums.length; j++) {
+                if(nums[i] > nums[j] && j != i) {
+                    count++;
+                }
+            }
             ans[i] = count;
-            return helper(nums, i + 1, 0, 0, ans);
         }
-        return helper(nums, i , count, j + 1, ans);
-
+        return ans;
     }
+
+    // public int[] helper(int[] nums, int i, int count, int j, int[] ans) {
+    //     if(i == nums.length) {
+    //         return ans;
+    //     }
+    //     if(nums[i] > nums[j] && nums[i] != nums[j]) {
+    //         count++;
+    //     }
+    //     if(j == nums.length - 1) {
+    //         ans[i] = count;
+    //         return helper(nums, i + 1, 0, 0, ans);
+    //     }
+    //     return helper(nums, i , count, j + 1, ans);
+
+    // }
 }
