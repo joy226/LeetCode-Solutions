145a64bd80cb8fe57d4f1af0e85e47967255043a
JOY <141978977+joy226@users.noreply.github.com>
2025-04-24 22:38:46 +0530

    Time: 235 ms (19%), Space: 45.1 MB (63.42%) - LeetHub

---
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int countCompleteSubarrays(int[] nums) {
+        ArrayList<Integer> unique = new ArrayList<>();
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            // Check if this element is included in result
+            int j;
+            for (j = 0; j < i; j++){
+                if (nums[i] == nums[j]){
+                    break;
+                }
+            }
+            // Include this element if not included previously
+            if (i == j){
+                unique.add(nums[i]);
+            }
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            HashSet<Integer> currentSubArray = new HashSet<Integer>();
+            for(int j = i; j < nums.length; j++) {
+                currentSubArray.add(nums[j]);
+                if(currentSubArray.size() == unique.size()) {
+                    count++;
+                }
+            }
+        }
+        return count;
+
+    }
+}
