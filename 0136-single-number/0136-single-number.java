52f55a5040e72136cbf89a03c01b5ec9b4e4494d
JOY <141978977+joy226@users.noreply.github.com>
2025-05-22 00:23:43 +0530

    Time: 360 ms (5.28%), Space: 46.5 MB (17.82%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+            if (count == 1) {
+                return nums[i];  // Return immediately when found
+            }
+        }
+        return -1; // Or throw an exception if no single number is found
+    }
+}
