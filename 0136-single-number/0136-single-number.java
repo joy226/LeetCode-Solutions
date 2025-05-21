ad68621741d952fe2c7d93b9ab9c1ae58814e664
JOY <141978977+joy226@users.noreply.github.com>
2025-05-22 00:26:00 +0530

    Time: 1 ms (99.93%), Space: 46.2 MB (46.88%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,16 +1,22 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            int count = 0;
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
-                    count++;
-                }
-            }
-            if (count == 1) {
-                return nums[i];  // Return immediately when found
-            }
+        // for (int i = 0; i < nums.length; i++) {
+        //     int count = 0;
+        //     for (int j = 0; j < nums.length; j++) {
+        //         if (nums[i] == nums[j]) {
+        //             count++;
+        //         }
+        //     }
+        //     if (count == 1) {
+        //         return nums[i];  // Return immediately when found
+        //     }
+        // }
+        // return -1; // Or throw an exception if no single number is found
+
+        int res = 0;
+        for (int num : nums) {
+            res ^= num;  // XOR cancels out numbers that appear twice
         }
-        return -1; // Or throw an exception if no single number is found
+        return res;
     }
 }
