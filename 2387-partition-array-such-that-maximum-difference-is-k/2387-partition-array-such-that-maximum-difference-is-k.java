e70ca57dcd0294221871d67de59566bc9b1d3441
JOY <141978977+joy226@users.noreply.github.com>
2025-06-19 23:13:30 +0530

    Time: 61 ms (5.32%), Space: 57.1 MB (40.07%) - LeetHub

---
+++ 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int partitionArray(int[] nums, int k) {
+        if(nums.length == 0) {
+            return 0;
+        }
+        int[] sortedNums = sort(nums);
+        int count  = 0;
+        long minVal = -1;
+        for(int i = 0; i < sortedNums.length; i++) {
+            int num = sortedNums[i];
+            if(count == 0 || (long)num - minVal > k) {
+                count++;
+                minVal = num;
+            }
+        }
+        return count;
+    }
+
+    public static int[] sort(int[] arr) {
+        if (arr.length == 1) {
+            return arr;
+        }
+        int mid = arr.length / 2;
+        int[] left = sort(Arrays.copyOfRange(arr, 0, mid));
+        int[] rigth = sort(Arrays.copyOfRange(arr, mid, arr.length));
+        return merge(left, rigth);
+    }
+
+    public static int[] merge(int[] first, int[] second) {
+        int[] mix = new int[first.length + second.length];
+        int i = 0; // pointer for first array
+        int j = 0; // pointer for second array
+        int k = 0; // pointer for mix array
+        while (i < first.length && j < second.length) {
+            if (first[i] < second[j]) {
+                mix[k] = first[i];
+                i++;
+            } else {
+                mix[k] = second[j];
+                j++;
+            }
+            k++;
+        }
+        // it will be possible that one of the array is mot complete then we need to add all the remaining element into the mix array.
+        while (i < first.length) {
+            mix[k] = first[i];
+            i++;
+            k++;
+        }
+        while (j < second.length) {
+            mix[k] = second[j];
+            j++;
+            k++;
+        }
+        return mix;
+    }
+}
