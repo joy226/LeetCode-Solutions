28b2c0e99ffa65933516c8b325788cd70f7591ed
JOY <141978977+joy226@users.noreply.github.com>
2026-07-05 16:49:02 +0530

    Time: 1 ms (100%), Space: 44 MB (91.79%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,16 +1,36 @@
 class Solution {
-    public boolean isPalindrome(String s) {
-        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
-        int start = 0;
-        int end = s.length() - 1;
-        while(start <= end) {
-            if(s.charAt(start) != s.charAt(end)) {
+    public boolean isPalindrome(String str) {
+        int s = 0;
+        int e = str.length() - 1;
+        while(s <= e) {
+            char start = str.charAt(s);
+            char end = str.charAt(e);
+
+            if(start >= 65 && start <= 90) {
+                start += (char) 32;
+            }
+
+            if(end >= 65 && end <= 90) {
+                end += (char) 32;
+            }
+
+            if( (start < 97 || start > 122 )&& (start < 48 || start > 57)) {
+                s++;
+                continue;
+            }
+
+            if( ( end < 97 || end > 122 ) && (end < 48 || end > 57)) {
+               e--;
+               continue;
+            }
+
+            if(start != end) {
                 return false;
             }
-            start++;
-            end--;
+
+            s++;
+            e--;
         }
         return true;
-
     }
 }
